3df9c453d9374a4319682d367672269a995b944f
L0rBl4k <lorblak@AppleM1.local>
2025-04-02 15:33:01 -0300

    refactor(Navbar.css, Navbar.js): removed className, added page-container on navbar, added responsive 'hamburguer' fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,30 +11,26 @@ const Navbar = () => {
   };
 
   return (
-    <div className="navbar-wrapper">
-      <nav className="navbar">
-        <div className="logo-container">
-          <Link to="/">
-            <img src={strawberryLogo} alt="Feria Kokoro Logo" className="logo" />
-          </Link>
-          <span className="logo-text">Feria Kokoro</span>
-        </div>
-
-        <div className={`links-container ${menuOpen ? "active" : ""}`}>
-          <Link to="/" className="link" onClick={() => setMenuOpen(false)}>INICIO</Link>
-          <Link to="/guests" className="link" onClick={() => setMenuOpen(false)}>INVITADOS</Link>
-          <Link to="/stands" className="link" onClick={() => setMenuOpen(false)}>STANDS</Link>
-          <Link to="/schedule" className="link" onClick={() => setMenuOpen(false)}>CRONOGRAMA</Link>
-          <Link to="/contests" className="link" onClick={() => setMenuOpen(false)}>CONCURSOS</Link>
-          <Link to="/gallery" className="link" onClick={() => setMenuOpen(false)}>GALERÍA</Link>
-        </div>
-
-        <div className={`menu-toggle ${menuOpen ? "open" : ""}`} onClick={toggleMenu}>
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
-        </div>
-      </nav>
+    <div className="page-container">
+    <nav className="navbar">
+      <div className="navbar-logo-container">
+        <img src={strawberryLogo} alt="Feria Kokoro Logo" className="navbar-image-logo" />
+        <span className="navbar-logo-text">Feria Kokoro</span>
+      </div>
+      <div className={`links-container ${menuOpen ? "active" : ""}`}>
+        <Link to="/" className="link" onClick={() => setMenuOpen(false)}>INICIO</Link>
+        <Link to="/guests" className="link" onClick={() => setMenuOpen(false)}>INVITADOS</Link>
+        <Link to="/stands" className="link" onClick={() => setMenuOpen(false)}>STANDS</Link>
+        <Link to="/schedule" className="link" onClick={() => setMenuOpen(false)}>CRONOGRAMA</Link>
+        <Link to="/contests" className="link" onClick={() => setMenuOpen(false)}>CONCURSOS</Link>
+        <Link to="/gallery" className="link" onClick={() => setMenuOpen(false)}>GALERÍA</Link>
+      </div>
+      <div className={`menu-toggle ${menuOpen ? "open" : ""}`} onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+    </nav>
     </div>
   );
 };
