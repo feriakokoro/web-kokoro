fe7bcc86b96b4e2538e0866cbd55063cd3c756e3
L0rBl4k <lorblak@AppleM1.local>
2025-04-02 15:53:55 -0300

    refactor(Navbar.css, Navbar.js): included code for blank space between navbar and background

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import strawberryLogo from "../assets/icons/strawberry-logo.webp";
 import "./Navbar.css";
+import "./global.css";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -11,27 +12,25 @@ const Navbar = () => {
   };
 
   return (
-    <div className="page-container">
-      <nav className="navbar">
-        <div className="navbar-logo-container">
-          <img src={strawberryLogo} alt="Feria Kokoro Logo" className="navbar-image-logo" />
-          <span className="navbar-logo-text">Feria Kokoro</span>
-        </div>
-        <div className={`links-container ${menuOpen ? "active" : ""}`}>
-          <Link to="/" className="link" onClick={() => setMenuOpen(false)}>INICIO</Link>
-          <Link to="/guests" className="link" onClick={() => setMenuOpen(false)}>INVITADOS</Link>
-          <Link to="/stands" className="link" onClick={() => setMenuOpen(false)}>STANDS</Link>
-          <Link to="/schedule" className="link" onClick={() => setMenuOpen(false)}>CRONOGRAMA</Link>
-          <Link to="/contests" className="link" onClick={() => setMenuOpen(false)}>CONCURSOS</Link>
-          <Link to="/gallery" className="link" onClick={() => setMenuOpen(false)}>GALERÍA</Link>
-        </div>
-        <div className={`menu-toggle ${menuOpen ? "open" : ""}`} onClick={toggleMenu}>
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
-        </div>
-      </nav>
-    </div>
+    <nav className="navbar">
+      <div className="navbar-logo-container">
+        <img src={strawberryLogo} alt="Feria Kokoro Logo" className="navbar-image-logo" />
+        <span className="navbar-logo-text">Feria Kokoro</span>
+      </div>
+      <div className={`links-container ${menuOpen ? "active" : ""}`}>
+        <Link to="/" className="link" onClick={() => setMenuOpen(false)}>INICIO</Link>
+        <Link to="/guests" className="link" onClick={() => setMenuOpen(false)}>INVITADOS</Link>
+        <Link to="/stands" className="link" onClick={() => setMenuOpen(false)}>STANDS</Link>
+        <Link to="/schedule" className="link" onClick={() => setMenuOpen(false)}>CRONOGRAMA</Link>
+        <Link to="/contests" className="link" onClick={() => setMenuOpen(false)}>CONCURSOS</Link>
+        <Link to="/gallery" className="link" onClick={() => setMenuOpen(false)}>GALERÍA</Link>
+      </div>
+      <div className={`menu-toggle ${menuOpen ? "open" : ""}`} onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+    </nav>
   );
 };
 
