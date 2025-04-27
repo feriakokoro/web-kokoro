3d64e35223c0aa75366d2bdd2f11c1b44c2e57f1
L0rBl4k <lorblak@AppleM1.local>
2025-04-27 13:27:38 -0300

    feat(gallery.js): se integra servicio para consumir

--- src/services/gallery.js
+++ src/services/gallery.js
@@ -8,16 +8,15 @@ class GalleryService {
   getData = async () => {
     try {
       const response = await fetch(
-        `${process.env.PUBLIC_URL}/data/gallery.json`
+        'https://backend-kokoro.onrender.com/gallery'
       );
       if (!response.ok) {
         throw new Error(RESULT_CODE_FAIL_DESC);
       }
 
-      const text = await response.text();
-      console.log(API_RESPONSE_DESC, text);
-      const data = JSON.parse(text);
-      return data.Gallery;
+      const data = await response.json();
+      console.log(API_RESPONSE_DESC, data);
+      return data;
     } catch (error) {
       console.error(API_FAIL_DESC, error);
       throw error;
