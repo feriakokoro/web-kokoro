01e84633518491769fb087b0ebb324fcd2f37761
L0rBl4k <lorblak@AppleM1.local>
2025-04-29 19:07:47 -0300

    feat: se agregó coleccionador a rutas de la página

--- src/config/routes.js
+++ src/config/routes.js
@@ -5,11 +5,13 @@ const Participants = React.lazy(() => import("../pages/Participants"));
 const Schedule = React.lazy(() => import("../pages/Schedule"));
 const Contests = React.lazy(() => import("../pages/Contests"));
 const Gallery = React.lazy(() => import("../pages/Gallery"));
+const Stamps = React.lazy(() => import("../pages/Stamps"));
 
 export const routes = [
   { path: "/", element: <Home />, label: "INICIO" },
   { path: "/participants", element: <Participants />, label: "PARTICIPANTES" },
   { path: "/schedule", element: <Schedule />, label: "CRONOGRAMA" },
   { path: "/contests", element: <Contests />, label: "ACTIVIDADES" },
+  { path: "/stamps", element: <Stamps />, label: "COLECCIONADOR" },
   { path: "/gallery", element: <Gallery />, label: "GALERÍA" },
 ];
---
+++ src/pages/Stamps.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Stamps = () => {
+  return (
+    <div className="page-container">
+      <div className="section-container">
+        <h1 className="title">COLECCIONADOR DE SELLOS</h1>
+        <p>¡Pronto estarán disponibles!</p>
+      </div>
+    </div>
+  );
+};
+export default Stamps;
