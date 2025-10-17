c6dfd7a79ea14cc1750dd14a181f4c67ff9d2a66
L0rBl4k <lorblak@AppleM1.local>
2025-10-16 23:57:22 -0300

    refactor: se modifica mensaje de modal, se agrega modal al inicio de web

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -11,7 +11,24 @@ const HeroSection = ({ earlyTicket = {} }) => {
   const handleModalClose = () => {
     setIsModalOpen(false);
   };
-  
+
+  {
+    /*
+        <h2>¡Bienvenides a Feria Kokoro: {earlyTicket.title}!</h2>
+        <p>
+          Precio de la entrada anticipada: {earlyTicket.price} en efectivo
+        </p>
+        <p>Lugar: {earlyTicket.place}</p>
+        <p>Con tu entrada obtenés {earlyTicket.promotion}.</p>
+        <h2>¡Bienvenides a Feria Kokoro: {earlyTicket.title}!</h2>
+        <p>
+          Precio de la entrada anticipada: {earlyTicket.price} en efectivo
+        </p>
+        <p>Lugar: {earlyTicket.place}</p>
+        <p>Con tu entrada obtenés {earlyTicket.promotion}.</p>
+    */
+  }
+
   return (
     <section className="hero">
       <div className="hero-overlay" aria-hidden="true"></div>
@@ -27,12 +44,20 @@ const HeroSection = ({ earlyTicket = {} }) => {
         </button>
       </div>
       <Modal isOpen={isModalOpen} onClose={handleModalClose}>
-        <h2>¡Bienvenides a Feria Kokoro: {earlyTicket.title}!</h2>
+        <h2>¡Bienvenides a Feria Kokoro: Edición Felices Fiestas!</h2>
+        <p>Nuestra próxima edición está en camino.</p>
         <p>
-          Precio de la entrada anticipada: {earlyTicket.price} en efectivo
+          Te esperamos el Domingo 14 de Diciembre, 12 a 19 hs, en el Colegio San
+          José.
         </p>
-        <p>Lugar: {earlyTicket.place}</p>
-        <p>Con tu entrada obtenés {earlyTicket.promotion}.</p>
+        <p>Pronto toda la info acá.</p>
+        <p>
+          También podés seguirnos en{" "}
+          <a href="https://www.instagram.com/feriakokoro/" target="_blank">
+            @feriakokoro
+          </a>{" "}
+          para no perderte nada.
+        </p>
       </Modal>
     </section>
   );
